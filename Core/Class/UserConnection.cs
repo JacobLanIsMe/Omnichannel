e898e8f9f4663624113ecd965783162352ac74d5
Administrator <Administrator@WORKVM1>
2025-08-01 10:04:36 +0800

    Add project files.

---
+++ Core/Class/UserConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Core.Class
+{
+    public class UserConnection
+    {
+        //public ObjectId Id { get; set; }
+        public string? UserId { get; set; }
+        public string? ConnectionId { get; set; }
+        public DateTime? ConnectedAt { get; set; }
+        public DateTime? DisconnectedAt { get; set; }
+    }
+}
---
+++ Core/Hub/ConversationHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Core.Hub
+{
+    public class ConversationHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Client connected: {Context.ConnectionId}");
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            Console.WriteLine($"Client disconnected: {Context.ConnectionId}");
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
